ec5b4e2bbaa9585fb3400910c78a8db3475f66e2
Missangela99 <angelikahardyn99@gmail.com>
2024-06-09 02:17:11 +0200

    Finished the game, site now clears the logs after either winning or losing

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 //Title
 
- console.log("Let's play a game of Paper, rock, scissors!")
+ console.log("Let's play a game of Paper, rock, scissors!");
 
 //Scores variables
 
-let humanScore = 0
-let computerScore = 0
+let humanScore = 0;
+let computerScore = 0;
+let round = 0;
 
 let newList = document.querySelector(".list");
-let resultList = document.querySelector(".result")
+let resultList = document.querySelector(".result");
 
 //Getting CPU choice
 
@@ -16,16 +17,16 @@ let computerLine = document.createElement("li");
 
     function getComputerChoice() {
         
-        let cpunumber = Math.random()
+        let cpunumber = Math.random();
 
         if (cpunumber < 0.33) {
-            return "Paper"
+            return "Paper";
 
         } else if(cpunumber > 0.33 && cpunumber < 0.66) {
-            return "Rock"
+            return "Rock";
 
         } else if(cpunumber >= 0.66) {
-            return "Scissors"
+            return "Scissors";
     }
 }
 
@@ -39,79 +40,84 @@ let computerLine = document.createElement("li");
             let newLine = document.createElement("li");
             
             newLine.textContent = 'You chose ' + button.id + '! And your opponent chose ' + getComputerChoice() 
-            newList.appendChild(newLine)
+            newList.appendChild(newLine);
 
-            const humanChoice = button.id
-            playRound(humanChoice)
+            const humanChoice = button.id;
+            playRound(humanChoice);
 
         });
         
         function playRound(humanChoice) {
     
             let scoreLine = document.createElement("li");
-            let computerChoice = getComputerChoice()
+            let computerChoice = getComputerChoice();
             
             if (humanChoice == "Paper" && computerChoice == "Rock") {
-                scoreLine.textContent = "You won!"
+                scoreLine.textContent = "You won!";
                 newList.appendChild(scoreLine);
-                return humanScore += 1
+                return humanScore += 1;
     
             } else if (humanChoice == "Rock" && computerChoice == "Scissors") {
-                scoreLine.textContent = "You won!"
+                scoreLine.textContent = "You won!";
                 newList.appendChild(scoreLine);
-                return humanScore += 1
+                return humanScore += 1;
             
             } else if (humanChoice == "Scissors" && computerChoice == "Paper") {
-                scoreLine.textContent = "You won!"
+                scoreLine.textContent = "You won!";
                 newList.appendChild(scoreLine);
-                return humanScore += 1
+                return humanScore += 1;
      
             } else if (humanChoice === computerChoice) {
-                scoreLine.textContent = "Draw!"
+                scoreLine.textContent = "Draw!";
                 newList.appendChild(scoreLine);
             
             } else if (humanChoice == "Rock" && computerChoice == "Paper") {
-                scoreLine.textContent = "You lost!"
+                scoreLine.textContent = "You lost!";
                 newList.appendChild(scoreLine);
-                return computerScore += 1
+                return computerScore += 1;
             
             } else if (humanChoice == "Scissors" && computerChoice == "Rock") {
-                scoreLine.textContent = "You lost!"
+                scoreLine.textContent = "You lost!";
                 newList.appendChild(scoreLine);
-                return computerScore += 1
+                return computerScore += 1;
             
             } else if (humanChoice == "Paper" && computerChoice == "Scissors") {
-                scoreLine.textContent = "You lost!"
+                scoreLine.textContent = "You lost!";
                 newList.appendChild(scoreLine);
-                return computerScore += 1
+                return computerScore += 1;
             
                 }
             }
 
-            playRound();
-/*
-                // 5 rounds logic
-        while (humanScore <  5 || computerScore < 5) {
-            playRound()
-            console.log("Your score: " + humanScore, "Cpu score: " + computerScore)
+    let buttonPress = document.querySelector("button")
+    buttonPress.addEventListener("click", () => {
 
-            if (humanScore == 5) { 
-                alert("You got 5 points, you win!")
-                console.log("You got 5 points, you win!")
-                break
-            } else if (computerScore == 5) {
-                alert("Computer got 5 points, you lost!")
-                console.log("Computer got 5 points, you lost!")
-                break
+    let resetButton = document.createElement("button");
+    resetButton.textContent = 'CLEAR'
+    resetButton.addEventListener("click", () => {
+        newList.textContent = '';
+    })
+        for (let i = 0; i < 7; i++) {
+            playRound();
+
+            if (humanScore === 5) {
+                alert("You won!");
+                humanScore = 0;
+                computerScore = 0;
+                newList.textContent = ''
+                break;
+
+            } else if (computerScore === 5) {
+                alert("You lost!");
+                humanScore = 0;
+                computerScore = 0;
+                newList.textContent = ''
+                break;
             }
         }
-*/
-    });
+    })
 
 
+            
 
-//Playing single round
-
-
-
-
+});
